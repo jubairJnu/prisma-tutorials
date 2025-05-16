db5d784b303e98404f6974c146c97139fe7f4e76
Abdullah Al Jubair <jubair2810@gmail.com>
2025-05-16 20:01:36 +0600

    added:find-deep-dive

--- src/find.ts
+++ src/find.ts
@@ -3,9 +3,41 @@ import { PrismaClient } from "../generated/prisma";
 const prisma = new PrismaClient();
 
 async function main() {
-  const findMovies = await prisma.movie.findMany();
+  // ! Find Many
+  // const findMovies = await prisma.movie.findMany();
 
-  console.log(findMovies, "movies");
+  // console.log(findMovies, "movies");
+
+  // ! find unique
+
+  // const findUnique = await prisma.user.findUnique({
+  //   where: {
+  //     id: "6ded4eaa-ad97-4af7-84e9-fae3e62c2f98",
+  //   },
+  //   include: {
+  //     watchList: true,
+  //   },
+  // });
+  // ! find first
+  // const findFirst = await prisma.user.findFirst({
+  //   where: {
+  //     password: "123456",
+  //   },
+  //   include: {
+  //     watchList: true,
+  //   },
+  // });
+  // ! find with error
+  const findFirst = await prisma.user.findUniqueOrThrow({
+    where: {
+      email: "shakil@example2.com",
+    },
+    include: {
+      watchList: true,
+    },
+  });
+
+  console.log(findFirst, "find data");
 }
 
 main();
