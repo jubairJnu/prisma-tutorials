b1fe2657129328b60ea6f27a905b0073a9d75ef8
Abdullah Al Jubair <jubair2810@gmail.com>
2025-06-17 09:16:55 +0600

    added:aggregate-part-2

--- src/aggregate.ts
+++ src/aggregate.ts
@@ -6,7 +6,7 @@ import { prisma } from "./utis/prismaClient";
 //? 2:  Retrive Each Movie Avg rating.
 //? 3: Which movie have how much rating sum.
 //? 4:  who gives min rating. which movie have max rating.
-//? 5:  top 3 avg rating movie.
+//? 5:  top 2 avg rating movie.
 //? 6:  give which movies avg rating is greater than 4
 
 //
@@ -30,12 +30,66 @@ async function main() {
 
   // ?3
 
+  // const result = await prisma.review.groupBy({
+  //   by: ["movieId"],
+  //   _sum: {
+  //     rating: true,
+  //   },
+  // });
+  //? 4
+  //   const result = await prisma.review.groupBy({
+  //     by: ["userId"],
+  //     _min: {
+  //       rating: true,
+  //     },
+  //   });
+  //   const result = await prisma.review.groupBy({
+  //     by: ["movieId"],
+  //     _max: {
+  //       rating: true,
+  //     },
+  //   });
+  //
+  //   ?6
+  //   const result = await prisma.review.groupBy({
+  //     by: ["movieId"],
+  //     _avg: {
+  //       rating: true,
+  //     },
+  //     having: {
+  //       rating: {
+  //         _avg: {
+  //           gte: 3,
+  //         },
+  //       },
+  //     },
+  //   });
+  //
+
+  // ?5
   const result = await prisma.review.groupBy({
     by: ["movieId"],
-    _sum: {
+    _avg: {
       rating: true,
     },
+    // having: {
+    //   rating: {
+    //     _avg: {
+    //       gte: 3,
+    //     },
+    //   },
+    // },
+    // 1, 2, 3
+
+    orderBy: {
+      _avg: {
+        rating: "asc",
+      },
+    },
+    take: 2,
+    // skip: 1,
   });
+
   console.log(result, "result");
 }
 
