78d315d85f24eddaba3d91e6813a0ecc05c6d963
Abdullah Al Jubair <jubair2810@gmail.com>
2025-06-17 08:50:49 +0600

    added:three-task

--- src/aggregate.ts
+++ src/aggregate.ts
@@ -4,16 +4,39 @@ import { prisma } from "./utis/prismaClient";
 
 //? 1:  Retrive How much review Exist. and give total reviews by each movie
 //? 2:  Retrive Each Movie Avg rating.
-//? 3: Which movie have how much review sum.
+//? 3: Which movie have how much rating sum.
 //? 4:  who gives min rating. which movie have max rating.
 //? 5:  top 3 avg rating movie.
 //? 6:  give which movies avg rating is greater than 4
 
 //
 async function main() {
-  const result = await prisma.review.count();
+  // 1
+  //   const result = await prisma.review.count();
 
-  console.log(result);
+  //   const result = await prisma.review.groupBy({
+  //     by: ["movieId"],
+  //     _count: { id: true },
+  //   });
+
+  // ? 2
+
+  //   const result = await prisma.review.groupBy({
+  //     by: ["movieId"],
+  //     _avg: {
+  //       rating: true,
+  //     },
+  //   });
+
+  // ?3
+
+  const result = await prisma.review.groupBy({
+    by: ["movieId"],
+    _sum: {
+      rating: true,
+    },
+  });
+  console.log(result, "result");
 }
 
 main();
